c2613d6f901fff8188e052e6a7abd1aefc68be33
haussjonas <haussjonas@tutanota.de>
2021-12-27 22:24:23 +0100

    add monero-javascript and reconfigure storybook to get the library working. add theme provider for mui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,43 @@
+const { ProvidePlugin } = require("webpack");
+
 module.exports = {
+  staticDirs: ["../public"],
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "storybook-addon-material-ui",
+    "@react-theming/storybook-addon",
   ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
+  },
+  // FIXME: Temporary fix for: https://github.com/facebook/create-react-app/issues/11756
+  webpackFinal: async (config, { configType }) => {
+    config.plugins.push(
+      new ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      })
+    );
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      child_process: false,
+      fs: false,
+      zlib: false,
+      buffer: require.resolve("buffer"),
+      url: require.resolve("url"),
+      assert: require.resolve("assert"),
+      crypto: require.resolve("crypto-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream-browserify"),
+    };
+
+    return config;
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,24 @@
-import { muiTheme } from "storybook-addon-material-ui";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+import tipxmrTheme from "../src/theme";
 
-export const decorators = [muiTheme()];
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = createTheme(tipxmrTheme)
+
+// FIXME: SB in combination with MUI themes isn't working as intended
+// https://github.com/react-theming/storybook-addon/issues/39
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
